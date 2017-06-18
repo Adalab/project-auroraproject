95a1113430a4881db56f34356b3a5559e7171e94
Sara Damian <sara.dse@gmail.com>
2017-06-18 12:43:06 +0200

    second call API

--- dashboard.js
+++ dashboard.js
@@ -1,26 +1,45 @@
 'use strict';
 
-var nameRequest = new XMLHttpRequest();
+var userRequest = new XMLHttpRequest();
 var greeting = document.getElementById("greeting");
 var avatar = document.getElementById("avatar");
 
-
-nameRequest.open ('GET', 'https://api.taiga.io/api/v1/users/me', true);
-nameRequest.setRequestHeader("Content-Type", "application/json");
-nameRequest.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem('token'));
-nameRequest.onload = function () {
-  if (nameRequest.status >= 200 && nameRequest.status < 400) {
-    var data = JSON.parse(nameRequest.responseText);
+userRequest.open ('GET', 'https://api.taiga.io/api/v1/users/me', true);
+userRequest.setRequestHeader("Content-Type", "application/json");
+userRequest.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem('token'));
+userRequest.onload = function () {
+  if (userRequest.status >= 200 && userRequest.status < 400) {
+    var data = JSON.parse(userRequest.responseText);
     greeting.innerHTML = "Hello, " + data.full_name;
     console.log(data);
-    avatar.innerHTML = data.photo;
+    avatar.innerHTML = '<img src="'+ data.photo +'">';
   } else {
     console.log("La respuesta del servidor ha devuelto un error");
   }
 };
 
-nameRequest.onerror = function() {
+userRequest.onerror = function() {
   console.log("Error al tratar de conectarse con el servidor");
 };
 
-nameRequest.send();
+userRequest.send();
+
+var projectsRequest = new XMLHttpRequest();
+
+projectsRequest.open ('GET', 'https://api.taiga.io/api/v1/users/me', true);
+projectsRequest.setRequestHeader("Content-Type", "application/json");
+projectsRequest.setRequestHeader("Authorization", "Bearer " + sessionStorage.getItem('token'));
+projectsRequest.onload = function () {
+  if (projectsRequest.status >= 200 && projectsRequest.status < 400) {
+    var data = JSON.parse(projectsRequest.responseText);
+    console.log(data.projects);
+  } else {
+    console.log("La respuesta del servidor ha devuelto un error");
+  }
+};
+
+projectsRequest.onerror = function() {
+  console.log("Error al tratar de conectarse con el servidor");
+};
+
+projectsRequest.send();
