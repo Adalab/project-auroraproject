47252b58bf82923f20e7436694c43d971a77811c
noeus90 <noeus90@gmail.com>
2017-06-21 20:47:27 +0200

    added modules

--- dashboard.js
+++ dashboard.js
@@ -8,8 +8,11 @@ var descriptionProject = document.getElementById("description-project");
 var teamProject = document.getElementById("team");
 var likesProject = document.getElementById("like");
 var watchProject = document.getElementById("watch");
+var modulesProject = document.getElementById("modules");
 var userStoriesProject = document.getElementById("number-stories");
 var issuesProject = document.getElementById("number-issues");
+var closedPoints = document.getElementById("closed-points");
+var totalPoints = document.getElementById("total-points");
 
 userRequest.open ('GET', 'https://api.taiga.io/api/v1/users/me', true);
 userRequest.setRequestHeader("Content-Type", "application/json");
@@ -18,7 +21,6 @@ userRequest.onload = function () {
   if (userRequest.status >= 200 && userRequest.status < 400) {
     var data = JSON.parse(userRequest.responseText);
     greeting.innerHTML = "Hello, " + data.full_name;
-    console.log(data);
     if (data.photo) {
       avatar.innerHTML = '<img src="'+ data.photo +'">';
     }
@@ -49,7 +51,6 @@ projectsRequest.onload = function () {
     teamProject.innerHTML = "Team: " + dataProject[0].members.length;
     likesProject.innerHTML = dataProject[0].total_fans;
     watchProject.innerHTML = dataProject[0].total_watchers;
-    console.log(dataProject[0].id);
     var projectId = dataProject[0].id;
     sessionStorage.setItem("projectID", JSON.stringify(dataProject[0].id));
   } else {
@@ -63,10 +64,56 @@ projectsRequest.onerror = function() {
 
 projectsRequest.send();
 
-console.log(JSON.parse(sessionStorage.getItem('user')));
-var idProject = JSON.parse(sessionStorage.getItem('projectId'));
+var modulesRequest = new XMLHttpRequest();
+modulesRequest.open ('GET', 'https://api.taiga.io/api/v1/projects?member=' + user.id, true);
+modulesRequest.setRequestHeader("Content-Type", "application/json");
+modulesRequest.setRequestHeader("Authorization", "Bearer " + JSON.parse(sessionStorage.getItem('token')));
+modulesRequest.setRequestHeader("x-disable-pagination", true);
+modulesRequest.onload = function () {
+  if (modulesRequest.status >= 200 && modulesRequest.status < 400) {
+    var data = JSON.parse(modulesRequest.responseText);
+    console.log(data[0].is_backlog_activated);
+    if (data[0].is_backlog_activated === true) {
+      modulesProject.innerHTML= "Modules: backlog, ";
+    } else {
+      console.log("no found");
+    }
+    if (data[0].is_epics_activated === true) {
+      modulesProject.innerHTML+= "epics,";
+    } else {
+      console.log("no found");
+    }
+    if (data[0].is_issues_activated === true) {
+      modulesProject.innerHTML+= "issues,";
+    } else {
+      console.log("no found");
+    }
+    if (data[0].is_kanban_activated === true) {
+      modulesProject.innerHTML+= "kanban,";
+    } else {
+      console.log("no found");
+    }
+    if (data[0].is_wiki_activated === true) {
+      modulesProject.innerHTML+= "wiki";
+    } else {
+      console.log("no found");
+    }
+    // modulesProject.innerHTML= "Modules: " + data.length;
+    // console.log(data.is_backlog_activated);
+  } else {
+    console.log("La respuesta del servidor ha devuelto un error");
+  }
+};
+
+modulesRequest.onerror = function() {
+  console.log("Error al tratar de conectarse con el servidor");
+};
+
+modulesRequest.send();
+
+var idProject = sessionStorage.getItem("projectID");
 var userStoriesRequest = new XMLHttpRequest();
-userStoriesRequest.open ('GET', 'https://api.taiga.io/api/v1/userstories?project='+ 201914, true);
+userStoriesRequest.open ('GET', 'https://api.taiga.io/api/v1/userstories?project='+ idProject, true);
 userStoriesRequest.setRequestHeader("Content-Type", "application/json");
 userStoriesRequest.setRequestHeader("Authorization", "Bearer " + JSON.parse(sessionStorage.getItem('token')));
 userStoriesRequest.setRequestHeader("x-disable-pagination", true);
@@ -74,7 +121,6 @@ userStoriesRequest.onload = function () {
   if (userStoriesRequest.status >= 200 && userStoriesRequest.status < 400) {
     var data = JSON.parse(userStoriesRequest.responseText);
     userStoriesProject.innerHTML= data.length;
-    console.log(data);
   } else {
     console.log("La respuesta del servidor ha devuelto un error");
   }
@@ -88,8 +134,6 @@ userStoriesRequest.send();
 
 var issuesRequest = new XMLHttpRequest();
 var idProject = sessionStorage.getItem("projectID");
-console.log("Proyecto");
-console.log(idProject);
 
 issuesRequest.open ('GET', 'https://api.taiga.io/api/v1/projects/' + idProject +'/issues_stats' , true);
 issuesRequest.setRequestHeader("Content-Type", "application/json");
@@ -97,10 +141,8 @@ issuesRequest.setRequestHeader("Authorization", "Bearer " + JSON.parse(sessionSt
 issuesRequest.setRequestHeader("x-disable-pagination", true);
 issuesRequest.onload = function () {
   if (issuesRequest.status >= 200 && issuesRequest.status < 400) {
-    console.log(issuesRequest.responseText);
     var data = JSON.parse(issuesRequest.responseText);
     issuesProject.innerHTML = data.total_issues;
-    console.log(data.total_issues);
   } else {
     console.log("La respuesta del servidor ha devuelto un error");
   }
@@ -113,3 +155,26 @@ issuesRequest.onerror = function() {
 };
 
 issuesRequest.send();
+
+var pointRequest = new XMLHttpRequest();
+pointRequest.open ('GET', 'https://api.taiga.io/api/v1/projects/'+ idProject +'/stats',true);
+console.log(idProject);
+pointRequest.setRequestHeader("Content-Type", "application/json");
+pointRequest.setRequestHeader("x-disable-pagination", true);
+pointRequest.onload = function () {
+  if (pointRequest.status >= 200 && pointRequest.status < 400) {
+    var data = JSON.parse(pointRequest.responseText);
+    closedPoints.innerHTML = data.closed_points;
+    totalPoints.innerHTML = data.defined_points;
+  } else {
+    console.log("La respuesta del servidor ha devuelto un error");
+  }
+};
+
+pointRequest.onerror = function() {
+
+  console.log("Error al tratar de conectarse con el servidor");
+
+};
+
+pointRequest.send();
